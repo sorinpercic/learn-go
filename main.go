e97c10b6da5d3b3789cd3f8f1615b1020f116d46
sorinpercic <sorinpercic806@gmail.com>
2025-04-05 19:37:02 +0300

    made the scraper to work on every page

--- main.go
+++ main.go
@@ -5,43 +5,56 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	//HTTP REQ
-	res, err := http.Get("https://quotes.toscrape.com/")
-
-	if err != nil {
-		log.Fatal(err)
-
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatalf("Error : %d %s", res.StatusCode, res.Status)
-	}
-	//LOAD HTML doc
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//CREATE CSV
 	file, err := os.Create("quotes.csv")
 	if err != nil {
 		log.Fatal("CSV file was not created, err: ", err)
 	}
 	defer file.Close()
+
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
 	writer.Write([]string{"Quote", "Author"})
-	doc.Find(".quote").Each(func(index int, s *goquery.Selection) {
-		quoteText := s.Find(".text").Text()
-		author := s.Find(".author").Text()
 
-		writer.Write([]string{quoteText, author})
-	})
-	log.Println("Saving data to CSV file")
+	page := 1
+
+	for {
+		url := "https://quotes.toscrape.com/page/" + strconv.Itoa(page) + "/"
+
+		res, err := http.Get(url)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != 200 {
+			log.Fatal(err)
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		doc.Find(".quote").Each(func(i int, s *goquery.Selection) {
+
+			quoteText := s.Find(".text").Text()
+			author := s.Find(".author").Text()
+
+			writer.Write([]string{quoteText, author})
+		})
+		nextPage := doc.Find(".next a").AttrOr("href", "")
+		if nextPage == "" {
+			break
+		}
+		page++
+	}
+	log.Println("Everything works fine... ")
 }
